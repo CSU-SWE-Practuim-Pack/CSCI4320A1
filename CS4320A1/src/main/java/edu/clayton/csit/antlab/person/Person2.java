007435751f755d59cd0c24436ca4532f86e65119
Latharcus <99180831+Latharcus@users.noreply.github.com>
2022-09-02 19:48:33 -0400

    Removed useless.p2.txt Added readme.p2
    Added code to method calc(String input) in person2.java.
    Created code to shuffle string pname into a random order
    Was able to successfully run the code to get pname in a random order.

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
@@ -1,4 +1,5 @@
 package edu.clayton.csit.antlab.person;
+import java.util.*;
 
 /**
  *  A simple class for person 2
@@ -31,7 +32,16 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		List<String> rearrange = Arrays.asList(input.split(""));
+		Collections.shuffle(rearrange);//method for shuffling strings
+		String random = "";
+
+		//for loop to iterate through rearrange
+		for (String letter : rearrange){
+			random += letter;
+		}
+
+	  return random;
 	}
 	/**
 	 * Return a string rep of this object
@@ -42,6 +52,7 @@ public class Person2 {
 	 *         object
 	 */
 	public String toString(String input) {
+		input = "gtg123b";
 	  return name + calc(input);
 	}
 }
