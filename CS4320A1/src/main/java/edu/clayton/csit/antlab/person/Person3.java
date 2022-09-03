434f0f49a2d5bd8551b9648f0d81ccfdf0ea8cfb
JP-01 <Jpatel38@student.clayton.edu>
2022-09-02 20:43:46 -0400

    Added method to Person3.java

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person3.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person3.java
@@ -43,6 +43,13 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+		String tmp = "";
+		for (int i=input.length()-1; i>=0; i--){
+			tmp = tmp + input.charAt(i);
+
+
+		}
+		System.out.print(tmp);
+		return tmp;
 	}
 }
